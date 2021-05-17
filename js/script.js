19a206f1cd8a6c4418fe3ff02eb7ac5506cc15c8
Krystian Kaźmierczak <s155774@gmail.com>
2021-05-17 20:25:34 +0200

    toggleTaskDone function added

--- js/script.js
+++ js/script.js
@@ -11,9 +11,7 @@
     ];
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
+        tasks.push({content: newTaskContent,});
         render();
     };
 
@@ -22,12 +20,18 @@
         render();
     }
 
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
-            htmlString +=` 
+            htmlString += ` 
             <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+            <button class="js-done">Zrobione</button>
             ${task.content}
             <button class="js-remove">Usuń</button>
             </li>
@@ -40,6 +44,14 @@
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+        
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
             });
         });
     }
